8c19f91ec6b9021aef333da6659d0245099101bb
Bea <beatrizfontanpadin@hotmail.com>
2023-11-09 17:33:29 +0100

    casa 09/11/23

--- 2.Objetos/Hola 2 - métodos estáticos/Ejercicio 1/Animal.java
+++ 2.Objetos/Hola 2 - métodos estáticos/Ejercicio 1/Animal.java
@@ -4,13 +4,43 @@ public class Animal {
     private String nome;
     private boolean vivo;
 
+    //Constructores
     public Animal(){
+        idade = 0;
+        nome = "desconocido";
+        vivo = true;
+    }
+
+    public Animal(int idad, String nom, boolean viv){
+        idade = idad;
+        nome = nom;
+        vivo = viv;
+    }
+
+    //Métodos
+    public void nace(){
+        if (vivo) {
+            vivo = true; // pongo el boolean a true, aunque ya está de base
+            System.out.println("Benvido");
+        }
+    }
+
+    public void morre() {
+  
+            vivo = false;
+            System.out.println("Adiós");
         
+    }
 
-
+    public int getDados() {
+        System.out.println("Nombre: " + nome + ", Edad: " + idade);
+        return idade;
     }
 
 
-
-
 }
+
+
+
+
+
