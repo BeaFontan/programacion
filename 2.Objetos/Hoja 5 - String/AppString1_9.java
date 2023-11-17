ba5e21d1ee19aab36ebf6b3d502d50e247725e60
Bea <a23BeatrizFP@iessanclemente.net>
2023-11-17 11:07:00 +0100

    programación

--- 2.Objetos/Hoja 5 - String/AppString1_9.java
+++ 2.Objetos/Hoja 5 - String/AppString1_9.java
@@ -1,3 +1,38 @@
 //9.O programa deberá ir pedindo cadeas de caracteres, sen puntos, e para cada unha delas cambiar todas
 //as letras 'a' por 'e', e todas as 'e' por 'a', e despois imprimir a cadea. O programa rematará se o tamaño da
 //cadea introducida é 2 (ten só 2 letras).
+
+import java.util.Scanner;
+
+public class AppString1_9 {
+
+    public static void main(String[] args) {
+        
+    Scanner teclado = new Scanner(System.in);
+    String respuesta;
+
+
+
+    System.out.println("Dime a frase");
+    respuesta = teclado.nextLine();
+
+
+    while (respuesta.length() > 2) { // mientras la frase introducida tenga más de 2 letras
+
+        respuesta = respuesta.replace('a', 'e');
+        respuesta = respuesta.replace('e', 'a'); //REVISAR ESTO QUE NO ME DA BIEN
+
+        System.out.println(respuesta);
+
+        System.out.println("Dime a frase");
+        respuesta = teclado.nextLine();
+        
+    }
+
+
+
+    teclado.close();
+
+
+    }
+}
